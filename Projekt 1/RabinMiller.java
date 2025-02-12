8f9788d6a78c94b79fde2366e1b62543ad72ddcb
Brian <br2571bu-s@student.lu.se>
2025-02-12 23:58:32 +0100

    Big Integer implemented in rabin Miller

--- Projekt 1/RabinMiller.java
+++ Projekt 1/RabinMiller.java
@@ -1,62 +1,64 @@
+import java.math.BigInteger;
 import java.util.Random;
 
 public class RabinMiller {
 
-    public boolean isPrime(int n, int k) {
+    public boolean isPrime(BigInteger n, int k) {
 
         // input n > 3, an odd integer to be tested for primality
 
-        if (n == 3) {
+        if (n.equals(BigInteger.valueOf(3))) {
             System.out.println("is Prime");
             return true;
         }
 
-        if (n == 2) {
+        if (n.equals(BigInteger.valueOf(2))) {
             System.out.println("is Prime");
             return true;
         }
 
-        if (n == 1) {
+        if (n.equals(BigInteger.valueOf(1))) {
             System.out.println("not Prime");
             return false;
         }
-        if (n % 2 == 0) {
-            System.out.println("is composite");
+        if (n.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))) {
+            System.out.println("is Composite");
             return false;
         }
 
         // Write n - 1 as 2^rs with s odd by factoring powers of 2 from n - 1
-        int s = n - 1;
+        BigInteger s = n.subtract(BigInteger.valueOf(1));
         int r = 0;
 
-        while (s % 2 == 0) {
-            s = s / 2;
+        while (s.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))) {
+            s = s.divide(BigInteger.valueOf(2));
             r++;
         }
 
-        // Pick random integer a in the range [2, n-2] k times
         Random rand = new Random();
 
         for (int i = 0; i < k; i++) {
-            int a = rand.nextInt(n - 2) + 2;
+            // Pick random integer a in the range [2, n-2] k times
+            BigInteger a = BigInteger.valueOf(rand.nextInt(n.intValue() - 2) + 2);
 
             // x = a^s mod n
-            int x = (int) Math.pow(a, s) % n;
+            BigInteger x = a.modPow(s, n);
 
             // if x = 1 or x = n-1 then return probably prime
-            if (x == 1 || x == n - 1) {
+            if (x.equals(BigInteger.valueOf(1)) || x.equals(n.subtract(BigInteger.valueOf(1)))) {
                 System.out.println("probablyPrime");
                 return true;
             }
 
             for (int j = 0; j < r - 1; j++) {
                 // x = a^s mod n
-                x = (int) Math.pow(a, Math.pow(2, j) * s) % n;
 
-                if (x == 1) {
+                x = x.modPow(BigInteger.valueOf(2), n);
+
+                if (x.equals(BigInteger.valueOf(1))) {
                     System.out.println("isComposite");
                     return false;
-                } else if (x == n - 1) {
+                } else if (x.equals(n.subtract(BigInteger.valueOf(1)))) {
                     System.out.println("ProbablyPrime");
                     return true;
                 }
