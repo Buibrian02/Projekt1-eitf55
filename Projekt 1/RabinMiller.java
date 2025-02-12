8ae0efd32ae0cc4befb140b1b1371251fb57eecc
Brian <br2571bu-s@student.lu.se>
2025-02-12 20:12:54 +0100

    updated rabin miller

--- Projekt 1/RabinMiller.java
+++ Projekt 1/RabinMiller.java
@@ -2,10 +2,26 @@ import java.util.Random;
 
 public class RabinMiller {
 
-    public boolean rabinMiller(int n, int k) {
+    public boolean isPrime(int n, int k) {
 
         // input n > 3, an odd integer to be tested for primality
-        if (n < 3) {
+
+        if (n == 3) {
+            System.out.println("is Prime");
+            return true;
+        }
+
+        if (n == 2) {
+            System.out.println("is Prime");
+            return true;
+        }
+
+        if (n == 1) {
+            System.out.println("not Prime");
+            return false;
+        }
+        if (n % 2 == 0) {
+            System.out.println("is composite");
             return false;
         }
 
@@ -18,31 +34,34 @@ public class RabinMiller {
             r++;
         }
 
-        // Pick random integer a in the range [2, n-2]
+        // Pick random integer a in the range [2, n-2] k times
         Random rand = new Random();
-        int a = rand.nextInt(n - 2) + 2;
 
-        // x = a^s mod n
-        int x = (int) Math.pow(a, s) % n;
+        for (int i = 0; i < k; i++) {
+            int a = rand.nextInt(n - 2) + 2;
 
-        // if x = 1 or x = n-1 then return probably prime
-        if (x == 1 || x == n - 1) {
-            System.out.println("probablyPrime");
-            return true;
-        }
-
-        // for j=1 to r-1
-        for (int j = 1; j < r - 1; j++) {
             // x = a^s mod n
-            x = (int) Math.pow(a, Math.pow(2, j) * s) % n;
-        }
+            int x = (int) Math.pow(a, s) % n;
 
-        if (x == 1) {
-            System.out.println("isComposite");
-            return false;
-        } else if (x == n - 1) {
-            System.out.println("ProbablyPrime");
-            return true;
+            // if x = 1 or x = n-1 then return probably prime
+            if (x == 1 || x == n - 1) {
+                System.out.println("probablyPrime");
+                return true;
+            }
+
+            for (int j = 0; j < r - 1; j++) {
+                // x = a^s mod n
+                x = (int) Math.pow(a, Math.pow(2, j) * s) % n;
+
+                if (x == 1) {
+                    System.out.println("isComposite");
+                    return false;
+                } else if (x == n - 1) {
+                    System.out.println("ProbablyPrime");
+                    return true;
+                }
+            }
+
         }
 
         System.out.println("isComposite");
